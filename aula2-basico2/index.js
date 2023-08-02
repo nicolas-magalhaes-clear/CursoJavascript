09c5bf2e13fef7f5110bddc4f2e84c2cb1d44cbc
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-02 12:11:45 -0300

    added: index.js, index.html

---
+++ aula2-basico2/index.js
@@ -0,0 +1,20 @@
+/*
+Funcoes, como usar
+*/
+
+//Tipos de funcoes
+//Comum, anônima, e arrow function
+//Comum pode chamar varias vezes, anonima nao pode ser chamada pelo nome.
+
+//Funcao comum
+function somar(valor1 , valor2){
+    return valor1 + valor2;
+}
+
+let numero1;
+let numero2;
+
+let resultado = somar(numero1, numero2);
+
+console.log(resultado);
+
