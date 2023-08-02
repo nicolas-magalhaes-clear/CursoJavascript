92ad45fe7624fbdc14a7b6bb59dbef38dd8661bb
ryangualbertoclear <ryan.oliveira.clear@gmail.com>
2023-08-02 11:46:48 -0300

    added: index.js

---
+++ aula1-basico1/LEFT/index.js
@@ -0,0 +1,11 @@
+let cor  = 'amarelo'
+
+if(cor === 'amarelo'){
+    console.log('Atenção!')
+}
+if(cor === 'verde'){
+    console.log('Espere');
+}
+if(cor === 'verde'){
+    console.log('Prossiga');
+}
