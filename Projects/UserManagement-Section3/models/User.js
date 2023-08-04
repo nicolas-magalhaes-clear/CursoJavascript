9da801fb209f4e404f20fc451edcc9d466d5aa7f
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-04 12:17:37 -0300

    added: created view users

---
+++ Projects/UserManagement-Section3/models/User.js
@@ -0,0 +1,14 @@
+class User{
+
+    constructor(name, gender, birthDate, country, email, password, photo, admin){
+     
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+    }
+}
