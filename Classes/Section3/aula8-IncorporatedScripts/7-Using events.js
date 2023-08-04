3c066dc505a46a962147e6679c815a5a360cb471
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-04 11:42:04 -0300

    added: explanation about preventDefault added

--- Classes/Section3/aula8-IncorporatedScripts/7-Using events.js
+++ Classes/Section3/aula8-IncorporatedScripts/7-Using events.js
@@ -17,3 +17,15 @@ const div1 = document.querySelector('.div1');
 div1.addEventListener('click', (e) => {
     console.log('I was clicked!');
 })
+
+/*
+Important note, we can prevent the default event of an element using the
+preventDefault()
+For example, buttons have a native event of submite when clicked, soo by using prevent default allow us to
+deny this action
+*/
+
+const buttonSelected = document.querySelector('#buttonDenied');
+buttonSelected.addEventListener('click', (e) => {
+    e.preventDefault();
+})
