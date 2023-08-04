0b5bc2e47782e21fb4764f32b30b166d853872ef
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-04 11:55:18 -0300

    added: Explanation about functions

---
+++ Classes/Section3/aula8-IncorporatedScripts/8-Functions.js
@@ -0,0 +1,12 @@
+/*
+
+Functions are structures that allow us to define a code area that can be used more than once
+and returns a value
+
+example of a function that returns the value of a number plus other:
+*/
+function onePlusOther(a, b){
+    return a + b;
+}
+let i = onePlusOther(1,4);
+console.log(i);
