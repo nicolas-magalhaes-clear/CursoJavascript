bf8d1933aa0dbdf2e00f38be135a02e05cbe18c4
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-07 10:15:42 -0300

    updated: added form-validation'

--- Projects/UserManagement-Section3/controllers/UserController.js
+++ Projects/UserManagement-Section3/controllers/UserController.js
@@ -83,8 +83,13 @@ class UserController {
     getValues(){
 
         let user = {};
-
+        let isValid = true;
         [...this.formEl.elements].forEach(function(field, index){
+
+            if(['name', 'email','password'].indexOf(field.name) > -1 && !field.value){
+                field.parentElement.classList.add('has-error')
+                isValid = false;
+            }
 
             if (field.name === "gender") {
     
@@ -104,6 +109,9 @@ class UserController {
     
         });
     
+        if(!isValid){
+            return false;
+        }
         return new User(
             user.name, 
             user.gender, 
