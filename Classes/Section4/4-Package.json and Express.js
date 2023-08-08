dc35e3a997ee9606b2aee15e41678a9933620764
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-08 11:13:59 -0300

    added: created 4-package.json and Express.js

---
+++ Classes/Section4/4-Package.json and Express.js
@@ -0,0 +1,20 @@
+/*
+What is NPM? is a packet manager responsible for install and uninstall packages in our project
+We can use it to control which librabies, packages and other stuff we are going to put in our code
+
+The file package.json is the principal file in nodeJS projetcs, it contains the scripts, the dependencies of our code
+the version of our project, it's literally the front door of our project or the glossarium
+
+First to start a npm project we have to put in our CMD/Console the following command
+"npm init"
+
+After, this we will follow a 'wizard form' filling the informations that is asked to us, like:
+-project name (everything in lowercase without blank spaces)
+-version
+-description
+entry point  (what archive will start the project)
+-testCommand (which command will be used to test the code)
+-gitrepository (auto configure git)
+
+
+*/
