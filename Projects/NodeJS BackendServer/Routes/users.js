472a974409461735ecdcbae463aa1c4d65c909e3
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-09 09:42:53 -0300

    update: added data-validation to route using express-validator

--- Projects/NodeJS BackendServer/Routes/users.js
+++ Projects/NodeJS BackendServer/Routes/users.js
@@ -1,3 +1,5 @@
+const { check, validationResult } = require("express-validator");
+
 let neDB = require('nedb');
 let db = new neDB({
     filename: 'users.db',
@@ -7,6 +9,7 @@ let db = new neDB({
 module.exports = (app)=>{
 
     let route = app.route('/users');
+    //GET is used to get data
     route.get((req, res) =>{
 
         db.find({}).sort({name:1}).exec((err, users)=>{
@@ -24,17 +27,70 @@ module.exports = (app)=>{
         
             
     });
-    route.post((req, res) =>{
-       
-        //Using neDB to insert a data
-        db.insert(req.body, (err, user) => {
+    
+    //alteração na syntax atualização do express-validator
+    route.post(
+        [
+          check("name", "O nome é obrigatório.").notEmpty(),
+          check("email", "Email inválido.").notEmpty().isEmail(),
+        ],
+        (req, res) => {
+          let errors = validationResult(req);
+     
+          if (!errors.isEmpty()) {
+            app.utils.error.send(errors, req, res);
+            return false;
+          }
+     
+          db.insert(req.body, (err, user) => {
+            if (err) {
+              app.utils.error.send(err, req, res);
+            } else {
+              res.status(200).json(user);
+            }
+          });
+        }
+      );
 
+    
+
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) =>{
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
             if(err){
                 app.utils.error.send(err, req, res)
             }
             else{
                 res.status(200).json(user);
             }
-        });
-    });
+        })
+    })
+    
+
+    //put is used to insert data
+    routeId.put((req, res) =>{
+        db.update({_id:req.params.id}, req.body, err=>{
+            if(err){
+                app.utils.error.send(err, req, res)
+            }
+            else{
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+        })
+    })
+
+    //DELETE is used to delete data
+    routeId.delete((req, res)=>{
+        
+        db.remove({_id: req.params.id}, {}, err=>{
+            if(err){
+                app.utils.error.send(err, req, res)
+            }
+            else{
+                res.status(200).json(Object.assign(req.params));
+            }
+        })
+    })
 };
