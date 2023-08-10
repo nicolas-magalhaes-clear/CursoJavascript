f603611a8e4ae6036a0d74e56f855a63b23e142f
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-10 11:58:17 -0300

    update: added method to control upload view upload timeLeft method added upload format time view method added

--- Projects/5-DropBox-Clone/app/public/src/controller/DropBoxController.js
+++ Projects/5-DropBox-Clone/app/public/src/controller/DropBoxController.js
@@ -1,3 +1,5 @@
+
+
 class DropBoxController{
 
 
@@ -7,12 +9,15 @@ class DropBoxController{
         this.inputFilesEl = document.querySelector('#files');
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.snackModalEl = document.querySelector('#react-snackbar-root');
+
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+        this.nameFileEl = this.snackModalEl.querySelector('.filename');
+        this.timeLeftEl = this.snackModalEl.querySelector('.timeleft');
         this.initEvents();
     }
 
 
     initEvents(){
-
 
         this.btnSendFileEl.addEventListener('click', event =>{
 
@@ -23,8 +28,99 @@ class DropBoxController{
         this.inputFilesEl.addEventListener('change', event => {
             
             console.log(event.target.file);
+            this.uploadTask(event.target.files);
+            this.modalShow();
 
-            this.snackModalEl.style.display = 'block';
+            this.inputFilesEl.value = '';
         })
+    }//end method initEvents
+
+
+    uploadTask(files){
+        let promises = [];
+
+        [...files].forEach(file=>{
+            
+            promises.push(new Promise((resolve, reject) => {
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+                    this.modalShow(false);
+                    try{
+                        resolve(JSON.parse(ajax.responseText));
+                    }
+                    catch (e){
+                        reject(e);
+                    }
+                };
+
+                ajax.onerror = event => {
+                    this.modalShow(false);
+                    reject(event);
+                }
+
+                ajax.upload.onprogress = event =>{
+                    
+
+                    this.uploadProgress(event, file);
+                    console.log(event);
+
+                }
+
+                let formData = new FormData();
+
+                formData.append('input-file', file)
+                this.startUploadTime = Date.now();
+                ajax.send(formData);
+
+            }))
+        })
+
+        return Promise.all(promises);
+
+    }
+
+    uploadProgress(event, file){
+
+
+        let timeSpent = Date.now() - this.startUploadTime
+        let loaded = event.loaded;
+        let total = event.total;
+        
+        let porcent = parseInt((loaded/total)*100)
+
+        let timeLeft = ((100 - porcent) * timeSpent / porcent)
+        this.progressBarEl.style.width = `${porcent}%`
+
+        this.nameFileEl.innerHTML = file.name;
+        this.timeLeftEl.innerHTML = this.formatTimeToHuman(timeLeft);
+
+    }
+
+    formatTimeToHuman(duration){
+        
+        let seconds = parseInt((duration / 1000) % 60)
+        let minutes = parseInt(duration / (1000 * 60) % 60)
+        let hours = parseInt((duration / (1000 * 60 * 60) % 60)% 24)
+
+        if(hours > 0){
+            return `${hours} horas, ${minutes}, minutos, ${seconds} segundos`;
+        }
+        if(minutes > 0){
+            return ` ${minutes}, minutos, ${seconds} segundos`;
+        }
+        if(seconds > 0){
+            return `${seconds} segundos`;
+        }
+        return '';
+
+    }
+
+    modalShow(show = true){
+
+        this.snackModalEl.style.display = (show) ? 'block' : 'none';
     }
 }
