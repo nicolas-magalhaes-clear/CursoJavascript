d7ba2106733c11c4df2db79cc7ceeacf7c76f15f
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-11 12:55:46 -0300

    update: route added

--- Projects/5-DropBox-Clone/app/routes/index.js
+++ Projects/5-DropBox-Clone/app/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var formidable = require('formidable');
+var fs = require('fs')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,6 +24,47 @@ router.post('/upload', (req, res) => {
 
   })
   
+})
+
+router.delete('/file', (req, res) => {
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  })
+
+  form.parse(req, (err, fields, files) => {
+
+    let pathObj = fields.path;  
+    console.log('pathObj:', pathObj);
+    console.log('pathObj type:', typeof(pathObj));
+
+    let path = pathObj[0].toString();
+    console.log('path', path);
+    console.log('path type', typeof(path))
+  
+
+
+    if(fs.existsSync(path)){
+      //Path exists, then deletes the file  
+      fs.unlink(path, (err) => {
+        if(err){
+          res.status(400).json({
+            err
+          });
+        }
+        else{
+          res.json({
+            files: path
+          });
+        }
+        
+      })
+    }
+    
+
+    
+
+  })
 })
 
 module.exports = router;
