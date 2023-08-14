94546fd777bcdc50c03a0f1436dcf330bbd64cdd
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-14 09:47:18 -0300

    update: added mechanism to change between directories

--- Projects/5-DropBox-Clone/app/public/src/controller/DropBoxController.js
+++ Projects/5-DropBox-Clone/app/public/src/controller/DropBoxController.js
@@ -3,6 +3,8 @@ constructor() {
 
 
     this.currentFolder = ['hcode']
+    this.navEl = document.querySelector('#browse-location');
+
     this.onSelectionChange = new Event('selectionchange');
 
     this.btnSendFileEl = document.querySelector("#btn-send-file");
@@ -21,7 +23,8 @@ constructor() {
 
     this.connectFirebase();
     this.initEvents();
-    this.readFiles();
+    this.openFolder();
+    
 }
 
 connectFirebase(){
@@ -240,8 +243,10 @@ uploadComplete() {
     this.btnSendFileEl.disabled = false
 }
 
-getFirebaseRef() {
-    return firebase.database().ref('files')
+getFirebaseRef(path) {
+
+    if(!path) path = this.currentFolder.join('/');
+    return firebase.database().ref(path)
 }
 
 modalShow(show = true) {
@@ -505,51 +510,128 @@ getFileView(file, key) {
 }
 
 readFiles() {
+
+    this.lastFolder = this.currentFolder.join('/');
+
     this.getFirebaseRef().on('value', snapshot => {
     this.listFilesEl.innerHTML = '';
     snapshot.forEach(snapshotItem => {
         
         let key = snapshotItem.key;
         let data = snapshotItem.val()
-                
-        this.listFilesEl.appendChild(this.getFileView(data, key))
+
+        if(data.type){
+            this.listFilesEl.appendChild(this.getFileView(data, key))
+        }
+
+        
     })
     })
 }
 
-initEventsLi(li) {
-    li.addEventListener('click', e => {
 
+openFolder(){
 
+    if(this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
     
+    this.renderNav();
 
-    
-    if (e.shiftKey) {
-        let firstLi = this.listFilesEl.querySelector('.selected');
+    this.readFiles();
+}
 
-        if (firstLi) {
-        let indexStart;
-        let indexEnd;
-        let lis = li.parentElement.childNodes;
+renderNav(){
 
-        lis.forEach((el, index) => {
-            if (firstLi === el) indexStart = index;
-            if (li === el) indexEnd = index;
-        })
-        
-        let index = [indexStart, indexEnd].sort()
+    let nav = document.createElement('nav');
+    let path = [];
+    for(let i = 0; i < this.currentFolder.length; i++){
 
-        lis.forEach((el, i) => {
-            if (i >= index[0] && i <= index[1]) {
-            el.classList.add('selected')
-            }
-        })
+        let folderName = this.currentFolder[i];
+        let span = document.createElement('span');
 
-        this.listFilesEl.dispatchEvent(this.onSelectionChange)
 
-        return true;
+        path.push(folderName)
+        if(i+1 === this.currentFolder.length){
+
+            span.innerHTML = folderName;
+
         }
+        else{
+
+            span.className = 'breadcrumb-segment__wrapper'
+            span.innerHTML = `
+            
+            <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
+            </span>
+            <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                <title>arrow-right</title>
+                <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+            </svg>
+                                        
+            `
+            
+        }
+        nav.appendChild(span);
+
     }
+    this.navEl.innerHTML = nav.innerHTML;
+    this.navEl.querySelectorAll('a').forEach(a => {
+        a.addEventListener('click', (e)=>{
+            e.preventDefault();
+            this.currentFolder = a.dataset.path.split('/');
+            this.openFolder()
+        })
+})
+
+
+}
+
+initEventsLi(li) {
+
+    li.addEventListener('dblclick', e=>{
+
+        let file = JSON.parse(li.dataset.file)
+        file = this.getDataType(file);
+
+        switch(file.type){
+
+            case 'folder':
+                this.currentFolder.push(file.name);
+                this.openFolder();
+
+                break
+            default:
+                window.open('/file?path=', file.path)
+
+        }
+    })
+    li.addEventListener('click', e => {
+        if (e.shiftKey) {
+            let firstLi = this.listFilesEl.querySelector('.selected');
+
+            if (firstLi) {
+            let indexStart;
+            let indexEnd;
+            let lis = li.parentElement.childNodes;
+
+            lis.forEach((el, index) => {
+                if (firstLi === el) indexStart = index;
+                if (li === el) indexEnd = index;
+            })
+            
+            let index = [indexStart, indexEnd].sort()
+
+            lis.forEach((el, i) => {
+                if (i >= index[0] && i <= index[1]) {
+                el.classList.add('selected')
+                }
+            })
+
+            this.listFilesEl.dispatchEvent(this.onSelectionChange)
+
+            return true;
+            }
+        }
 
     if (!e.ctrlKey) {
         this.listFilesEl.querySelectorAll('li.selected').forEach(el => {
