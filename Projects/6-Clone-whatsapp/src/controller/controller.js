5295bd7c956b51b7f329875db74ccff673afa013
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-15 12:43:56 -0300

    update: added mechanism to insert emojis into message

--- Projects/6-Clone-whatsapp/src/controller/controller.js
+++ Projects/6-Clone-whatsapp/src/controller/controller.js
@@ -353,6 +353,20 @@ class WhatsappController{
 
             emoji.on('click', e=>{
                 console.log(emoji.dataset.unicode);
+
+                let img = this.el.imgEmojiDefault.cloneNode();
+
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode
+
+                emoji.classList.forEach(name => {
+                    img.classList.add(name);
+                })
+
+                this.el.inputText.appendChild(img)
+
+                this.el.inputText.dispatchEvent(new Event('keyup'))
             })
 
         })
