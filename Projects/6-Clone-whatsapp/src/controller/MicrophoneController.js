5db38d42926c9fac6e4cf5468825e4f162f483c0
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-16 12:30:40 -0300

    update: renamed class to MicrophoneController

---
+++ Projects/6-Clone-whatsapp/src/controller/MicrophoneController.js
@@ -0,0 +1,36 @@
+
+import { ClassEvent } from "../utils/ClassEvent";
+
+export class MicrophoneController extends ClassEvent{
+
+
+    
+
+    constructor(){
+
+        super();
+
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then(stream=>{
+
+            this._stream = stream;
+
+            let audio = new Audio();
+            
+            audio.srcObject = stream;
+
+            audio.play()
+            
+            this.trigger('play', audio);
+            
+        }).catch(err =>{
+            console.log('err', err)
+        })
+    }
+
+    
+
+    
+}
+
