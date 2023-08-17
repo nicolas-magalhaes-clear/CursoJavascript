b235341eb95c022bbb0a9693d76abc8942e97181
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-17 12:54:26 -0300

    update: commit removed

--- Classes/Section1/aula1-basico1/LEFT/index.js
+++ Classes/Section1/aula1-basico1/LEFT/index.js
@@ -1,10 +1,14 @@
+<<<<<<< HEAD:Classes/Section1/aula1-basico1/LEFT/index.js
 let cor  = 'verde'
+=======
+let cor  = 'amarelo'
+>>>>>>> parent of 8b60017... changed: variable color value:aula1-basico1/LEFT/index.js
 
 if(cor === 'amarelo'){
     console.log('Atenção!')
 }
-if(cor === 'vermelho'){
-    console.log('pare');
+if(cor === 'verde'){
+    console.log('Espere');
 }
 if(cor === 'verde'){
     console.log('Prossiga');
