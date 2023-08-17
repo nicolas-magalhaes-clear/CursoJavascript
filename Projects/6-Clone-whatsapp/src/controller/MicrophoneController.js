12bf5542163df0373937b0bd726eaf182bd090b7
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-17 09:54:28 -0300

    update: added filereader to listen audio recorded

--- Projects/6-Clone-whatsapp/src/controller/MicrophoneController.js
+++ Projects/6-Clone-whatsapp/src/controller/MicrophoneController.js
@@ -10,26 +10,103 @@ export class MicrophoneController extends ClassEvent{
 
         super();
 
+        this._available = false;
+
+        this.FileDone;
+
+        this._mimetype = 'audio/webm'
+
         navigator.mediaDevices.getUserMedia({
             audio: true
         }).then(stream=>{
 
+            this._available = true;
+            
             this._stream = stream;
-
-            let audio = new Audio();
-            
-            audio.srcObject = stream;
-
-            audio.play()
-            
-            this.trigger('play', audio);
+                        
+            this.trigger('ready', this._stream);
             
         }).catch(err =>{
             console.log('err', err)
         })
     }
 
+
+    isAvailable(){
+        return this._available;
+    }
     
+    stop(){
+
+        this._stream.getTracks().forEach(track => {
+                track.stop();
+        })
+
+    }
+
+
+    startRecorder(){
+     
+        if(this.isAvailable()){
+
+            this._mediaRecorder = new MediaRecorder(this._stream, {
+                mimeType: this._mimetype
+            });
+
+            this._recordedChunks = [];
+
+            this._mediaRecorder.addEventListener('dataavailable', e=>{
+
+                if(e.data.size > 0) this._recordedChunks.push(e.data);
+
+            })
+
+            this._mediaRecorder.addEventListener('stop', e=> {
+
+                let blob = new Blob(this._recordedChunks, {
+                    this: this._mimetype
+                })
+
+                let filename = `rec${Date.now()}.webm`
+
+                let file = new File([blob], filename, {
+                    type: this._mimetype,
+                    lastModified: Date.now()
+                })
+
+                console.log('file', file)
+
+                let reader = new FileReader();
+
+                reader.onload = e =>{
+
+                        let audio = new Audio(reader.result);
+                        audio.src = reader.result                
+                        audio.play();
+                    
+                }
+
+                reader.readAsDataURL(file);
+            })
+            this._mediaRecorder.start()
+        }
+
+    }
+
+    stopRecorder(){
+
+
+        if(this.isAvailable()){
+
+            this._mediaRecorder.stop();
+            this.stop()
+        }
+        
+    }
+
+    startTimer(){
+        
+    }
 
     
 }
