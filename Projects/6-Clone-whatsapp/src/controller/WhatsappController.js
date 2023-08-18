66157de0aee365b8983da0c047a41fc45ceaffb6
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-18 08:55:27 -0300

    update: method init auth implemented now every update is auto reloaded

--- Projects/6-Clone-whatsapp/src/controller/WhatsappController.js
+++ Projects/6-Clone-whatsapp/src/controller/WhatsappController.js
@@ -29,20 +29,43 @@ export class WhatsappController{
         
         this._firebase.initAuth().then((response) => {
            
-            this._user = response.user;
+            this._user = new User(response.user.email);
 
-            let userRef = User.findByEmail(response.user.email)
+            this._user.on('datachange', data =>{
 
-            userRef.set({
-                name: response.user.displayName,
-                email: response.user.email,
-                photo: response.user.photoURL
+                document.querySelector('title').innerHTML = data.name + ' - Whatsapp Clone'
+                
+                this.el.inputNamePanelEditProfile.innerHTML = data.name
+
+                if(data.photo){
+                    let photo = this.el.imgPanelEditProfile;
+                    photo.src = data.photo;
+                    photo.show();
+                    this.el.imgDefaultPanelEditProfile.hide();
+
+                    let photo2 = this.el.myPhoto.querySelector('img');
+                    photo2.src = data.photo;
+                    photo2.show();
+                }
+            })
+
+            this._user.name = response.user.displayName;
+            this._user.email = response.user.email;
+            this._user.photo = response.user.photoURL;
+
+            this._user.save().then(()=>{
+                this.el.appContent.css({
+                    display: 'flex'
+                });
+            }).catch(error =>{
+                console.error(error);
             })
             
 
-            this.el.appContent.css({
-                display: 'flex'
-            });
+            
+            
+
+            
 
         }).catch(err=>{
             console.error(err);
@@ -224,7 +247,15 @@ export class WhatsappController{
             }
         })
         this.el.btnSavePanelEditProfile.on('click', e=>{
-            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+            this.el.btnSavePanelEditProfile.disabled = true;
+
+            this._user.name = this.el.inputNamePanelEditProfile.innerHTML;
+
+            this._user.save().then(()=>{
+                this.el.btnSavePanelEditProfile.disabled = false;
+            })
+            
         })
         this.el.formPanelAddContact.on('submit', e=>{
             e.preventDefault();
--- Projects/6-Clone-whatsapp/src/model/User.js
+++ Projects/6-Clone-whatsapp/src/model/User.js
@@ -1,13 +1,55 @@
 import { Firebase } from "../utils/Firebase";
-import { ClassEvent } from "../utils/ClassEvent";
+import { Model } from "./Model";
 
-export class User extends ClassEvent{
+export class User extends Model{
 
 
-    constructor(){
+    constructor(id){
 
+        super();
+
+        if(id) this.getById(id);
+        
     }
 
+
+    get name(){return this._data.name;}
+    set name(value){ this._data.name = value}
+    
+    get email(){return this._data.email;}
+    set email(value){ this._data.name = value}
+
+    get photo(){return this._data.photo;}
+    set photo(value){ this._data.name = value}
+
+    getById(id){
+        return new Promise((s,f)=>{
+            
+            User.findByEmail(id).onSnapshot(doc=>{
+                this.fromJSON(doc.data())
+
+                s(doc)
+            })
+            
+/*
+            User.findByEmail(id).get().then((doc) =>{
+
+                this.fromJSON(doc.data())
+
+                s(doc)
+            }).catch(err=>{
+                console.error(err);
+                f(err)
+            })
+            */
+
+        })
+    }
+
+    save(){
+
+        return User.findByEmail(this.email).set(this.toJSON());
+    }
 
     static getRef(){
 
