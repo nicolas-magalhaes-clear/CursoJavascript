14af99ffd02c6bce543fe5aa3f6735c0a9ddcb71
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-28 09:15:43 -0300

    update: template system added now any component can be renderized alone pages templates created more efficiently

--- Projects/7-Flavoured-restaurant/saboroso/routes/index.js
+++ Projects/7-Flavoured-restaurant/saboroso/routes/index.js
@@ -2,25 +2,65 @@ var express = require('express');
 
 var conn = require('./../inc/db')
 var router = express.Router();
+var menus = require('./../inc/menus')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  conn.query(`SELECT * FROM tb_menus ORDER BY title`,
-  (err, results)=>{
-    if(err){
-      console.log('Error:', err)
-    }
-    else{
-      console.log('Results:', results)
-      res.render('index',
-      { 
-        title: 'Restaurante Saboroso',
-        menus: results
-      });
-    }
+
+  menus.getMenus().then(results=>{
+
+    res.render('index',
+    {
+      title: 'Restaurante Saboroso',
+      menus: results
+    });
+
+  })
+
+  
+
+});
+
+
+router.get('/contacts', function (req, res) {
+
+  res.render('contacts', {
+    title: 'Contato - Restaurante Saboroso!',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi contato!'
+  })
+})
+
+router.get('/menus', function (req, res) {
+
+  menus.getMenus().then(results=>{
+
+    res.render('menus', {
+      title: 'Menus - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menus',
+      menus: results
+    });
+
   })
   
-});
+})
+
+router.get('/reservations', function (req, res) {
+  res.render('reservations', {
+    title: 'Reservas - Restaurante Saboroso!',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma mesa'
+  })
+})
+
+router.get('/services', function (req, res) {
+  res.render('services', {
+    title: 'Serviços - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É uma prazer poder servir'
+  })
+})
 
 module.exports = router;
