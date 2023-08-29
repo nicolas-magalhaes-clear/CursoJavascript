584b049724de5a9d9632956b8382dbd5296dcbce
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-08-29 09:36:27 -0300

    update: refactoring headers and footer in ejs include admin.js file added method to control active menu added added parameter menus to routes in admin.js

--- Projects/7-Flavoured-restaurant/saboroso/routes/admin.js
+++ Projects/7-Flavoured-restaurant/saboroso/routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var users = require('./../inc/users');
+var admin = require('./../inc/admin')
 
 router.use(function(req, res, next){
 
@@ -13,6 +14,13 @@ router.use(function(req, res, next){
 })
 
 
+router.use(function(req,res, next){
+
+    req.menus = admin.getMenus(req);
+
+    next();
+})
+
 router.get('/logout', function(req, res, next){
 
     delete req.session.user;
@@ -23,7 +31,9 @@ router.get('/logout', function(req, res, next){
 
 router.get('/', function(req, res, next){
 
-    res.render("admin/index")
+    res.render("admin/index", {
+        menus: req.menus
+    })
     
     
 });
@@ -59,14 +69,20 @@ router.get('/login', function(req, res, next){
 router.get('/contacts', function(req, res, next){
 
     res.render('admin/contacts', {
-
+        menus: req.menus
     })
 });
+
+router.get('/menus', function(req,res, next){
+    res.render('admin/menus', {
+        menus: req.menus
+    })
+})
 
 router.get('/emails', function(req, res, next){
 
     res.render('admin/emails', {
-
+        menus: req.menus
     })
 });
 
@@ -74,14 +90,15 @@ router.get('/reservations', function(req, res, next){
 
 
     res.render('admin/reservations', {
-        date: {}
+        date: {},
+        menus: req.menus
     })
 });
 
 router.get('/users', function(req, res, next){
 
     res.render('admin/users', {
-
+        menus: req.menus
     })
 });
 
